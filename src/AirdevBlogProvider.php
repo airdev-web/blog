c745f74b1fc5fdf26e992528a7685c9a35be7594
Romain Vause <romain.vause@gmail.com>
2021-07-14 19:36:19 +0200

    Initial commit

---
+++ src/AirdevBlogProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Airdev\Blog;
+
+use Illuminate\Support\ServiceProvider;
+
+class AirdevBlogProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__.'/routes.php';
+    }
+}
